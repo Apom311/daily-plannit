75a7f8533a13d2c42833f3d9306e9668c13af21a
Apon wuz here <frost_bird@hotmail.com>
2022-07-14 14:23:26 -0500

    fixed single digit hours from not pulling local storage

--- script.js
+++ script.js
@@ -21,23 +21,23 @@ setInterval(displayTime, 1000);
 console.log(currentHour);
 console.log(currentDay);
 
-var eightAm = $("#8");
-var nineAm = $("#9");
-var tenAm = $("#10");
-var elevenAm = $("#11");
-var twelvePm = $("#12");
-var onePm = $("#13");
-var twoPm = $("#14");
-var threePm = $("#15");
-var fourPm = $("#16");
-var fivePm = $("#17");
+var eightAm = $("#8a");
+var nineAm = $("#9a");
+var tenAm = $("#10a");
+var elevenAm = $("#11a");
+var twelvePm = $("#12p");
+var onePm = $("#13p");
+var twoPm = $("#14p");
+var threePm = $("#15p");
+var fourPm = $("#16p");
+var fivePm = $("#17p");
 
 function initPage() {
 
-  var init8 = JSON.parse(localStorage.getItem("08:00 am"))
+  var init8 = JSON.parse(localStorage.getItem("8:00 am"))
   eightAm.val(init8);
 
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+  var init9 = JSON.parse(localStorage.getItem("9:00 am"));
   nineAm.val(init9);
 
   var init10 = JSON.parse(localStorage.getItem("10:00 am"))
@@ -49,20 +49,22 @@ function initPage() {
   var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
   twelvePm.val(init12);
   
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  var init1 = JSON.parse(localStorage.getItem("1:00 pm"))
   onePm.val(init1);
   
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  var init2 = JSON.parse(localStorage.getItem("2:00 pm"))
   twoPm.val(init2);
   
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  var init3 = JSON.parse(localStorage.getItem("3:00 pm"))
   threePm.val(init3);
  
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  var init4 = JSON.parse(localStorage.getItem("4:00 pm"))
   fourPm.val(init4);
   
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  var init5 = JSON.parse(localStorage.getItem("5:00 pm"))
   fivePm.val(init5);
+
+  return;
 } 
 
 var userInput;
@@ -77,6 +79,7 @@ $(document).ready(function(){
     hourSpan = $(this).siblings(".input-group-prepend").text().trim();
     console.log(hourSpan);
     localStorage.setItem(hourSpan, JSON.stringify(userInput));
+    console.log(userInput)
 });})
 
 console.log(userInput);
